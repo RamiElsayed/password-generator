22d325135a0bb0ccd8460a17e904401fda24d423
Rami Elsayed <ramielsayed123@outlook.com>
2022-04-22 10:50:38 +0100

    password criteria has been set

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let specialChar = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 let numbers = "0123456789";
 let criterias = [];
-
+let passwordLength;
 const getPasswordLength = () => {
   let input = prompt(
     "Please enter the number of characters you want for your passwrod between 8 and 125: "
@@ -14,50 +14,49 @@ const getPasswordLength = () => {
   let parsed = parseInt(InputString);
   
     if (parsed && parsed >= 8 && parsed < 125) {
-       confirm("numer of characters is: " + parsed);
+       confirm("number of characters is: " + parsed);
        return parsed;
     } else {
-      alert("Please enter a valid numer");
+      alert("Please enter a valid number e.g(12)");
       getPasswordLength();
     }
 };
-
 const getPasswordCriteria = () => {
+  let criteria = "y";
   let firstCriteria = prompt("Would you like lower case charachters ? Y/N").toString().toLowerCase();
-  if (firstCriteria === "y") {
+  if (firstCriteria === criteria) {
     criterias.push(lowerCase);
   }
-  else if (firstCriteria === "n") {
-     confirm("Password will have no lower case characters");
-  }
-  else {
-    alert("Please only type Y for yes or N for no")
-  }
-  let secondCriteria = prompt("Would you like lower case charachters ? Y/N").toString().toLowerCase();
-  if (secondCriteria === "y") {
-    criterias.push(lowerCase);
+  let secondCriteria = prompt("Would you like upper case charachters ? Y/N").toString().toLowerCase();
+  if (secondCriteria === criteria) {
+    criterias.push(upperCase);
   }
   let thirdCriteria = prompt("Would you like special charachters ? Y/N").toString().toLowerCase();
-  if (thirdCriteria === "y") {
-    criterias.push(lowerCase);
+  if (thirdCriteria === criteria) {
+    criterias.push(specialChar);
   }
-  let forthCriteria = prompt("Would you like special charachters ? Y/N").toString().toLowerCase();
-  if (forthCriteria === "y") {
-    criterias.push(lowerCase);
+  let forthCriteria = prompt("Would you like numbers ? Y/N").toString().toLowerCase();
+  if (forthCriteria === criteria) {
+    criterias.push(numbers);
   }
-  else if (criterias.length < 0) {
-    alert("You need to choose at least one criteria ")
-    return  getPasswordCriteria();
-  }
-  [
-    "abcdefghijklmnopqrstuvwxyz",
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "0123456789",
-    " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
-  ];
+  else if (criterias.length < 1) {
+    alert("You need to choose at least one criteria and make sure type in only Y for yes or N for no");
+    getPasswordCriteria();
+  } 
+      confirm("Your password will be a random combination of " + criterias);
+      return criterias;
+  
 };
-
-const createRandomPassword = () => {};
+let result = "";
+const createRandomPassword = (length,criterias) => {
+  let i = 0;
+  while (i < length) {
+    result += criterias[i]
+    i++
+  }
+  return result;
+ 
+};
 
 //main function to generate the password
 const generatePassword = () => {
@@ -69,7 +68,8 @@ const generatePassword = () => {
   //create random password
   const password = createRandomPassword(passwordLength, PasswordCriteria);
 
-  return password;
+  //return password;
+  console.log(password)
 };
 
 // Write password to the #password input
